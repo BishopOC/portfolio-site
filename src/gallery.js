35ff290a955a0aa89d42ae71986167bbf5cdaf43
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-10-07 17:14:38 -0400

    roughed out the gallery page

--- src/gallery.js
+++ src/gallery.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import './css/gallery.css';
 
 class Gallery extends Component {
+
   render() {
     return (
       <div className='gallery'>
         <div className='gallery-items'>
-          <a href='#'>Recipe Keeper</a>
-          <a href='#'>Weather Data App</a>
-          <a href='#'>Fish Hawk Spirits</a>
-          <a href='#'>Youtube Mimic</a>
-          <a href='#'>Calculator</a>
+          <a className='recipe-link' href='#'>
+            <p> Recipe Book </p>
+          </a>
+          <a className='weather' href='#'>Weather Data App</a>
+          <a className='fishhawk' href='#'>Fish Hawk Spirits</a>
         </div>
       </div>
     );
