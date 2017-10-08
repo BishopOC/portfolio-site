836a2b76c54c273212274f8e2279bb1039381f35
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-10-07 20:23:44 -0400

    redesigned the gallery page

--- src/gallery.js
+++ src/gallery.js
@@ -7,11 +7,18 @@ class Gallery extends Component {
     return (
       <div className='gallery'>
         <div className='gallery-items'>
-          <a className='recipe-link' href='#'>
-            <p> Recipe Book </p>
+          <ul className='gallery-titles'>
+            <li>Recipe Book</li>
+            <li>Weather App</li>
+            <li className='fishhawk-title'>Fish Hawk Spirits</li>
+          </ul>
+          <a className='recipe-link' href='#'>Need a place to keep your recipes?
+          Sign up and start filling your recipe book today!
           </a>
-          <a className='weather' href='#'>Weather Data App</a>
-          <a className='fishhawk' href='#'>Fish Hawk Spirits</a>
+          <a className='weather' href='#'><p>Tired of doing a quick google
+          search for weather? Click here to do the same thing!</p></a>
+          <a className='fishhawk' href='#'><p>A Floridian distillery the produces
+          the finest spirits.</p></a>
         </div>
       </div>
     );
